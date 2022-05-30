572fc71e6e832dffe92129955747de0df5c03968
Olawale <olapetanwale@outlook.com>
2022-05-30 12:22:33 +0100

    Update README file and Fix linter errors

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 import removeBook from './modules/removeBook.js';
 import library from './modules/library.js';
 import manageDate from './modules/displayDate.js';
 import createUI from './modules/displayUI.js';
-import addbooks from './modules/addBooks.js'
-import navigation from './modules/navigation.js'
+import addbooks from './modules/addBooks.js';
+import navigation from './modules/navigation.js';
 
 library.books.forEach((element) => createUI(element));
 addbooks();
